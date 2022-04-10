2493afc5a22bcb803f5e7d665aebc5c0172a46b9
Ash Monty <git@ashmonty.com>
2022-04-10 16:31:53 +0200

    feat: cleaner, faster code + new source for user info

--- shield-generator/pages/api/shield/[user].js
+++ shield-generator/pages/api/shield/[user].js
@@ -1,154 +1,96 @@
 export default async function handler(req, res) {
-  // We have to return a promise. otherwise we get stalled requests since the API resolves without sending a response
+  // return a promise to avoid stalled requests
   return new Promise((resolve) => {
-    // This function gets the user information from the Discord API
-    async function getUserInfo(userID) {
-      const rawUserInfo = await fetch(
-        `http://167.71.241.147:3581/md-shield/json/?u=${userID}`
-      );
+    let { logoColor, style, theme, presenceTheme, compact, user, bot } =
+      req.query;
 
+    // presenceTheme is present for legacy purposes, theme should be used instead
+    theme = theme || presenceTheme;
+    style = style || "for-the-badge";
+
+    async function getUserInfo() {
+      // try fetching from my selfhosted bot
+      let rawUserInfo = await fetch(
+        `https://discord-md-badge.ashmonty.com/md-shield/${
+          bot ? "bot/" : ""
+        }json?u=${user}`
+      );
+      // If that fails, try fetching from my droplet
+      if (!rawUserInfo.ok) {
+        rawUserInfo = await fetch(
+          `http://167.71.241.147:3581/md-shield/${
+            bot ? "bot/" : ""
+          }json?u=${user}`
+        );
+      }
       const userInfo = await rawUserInfo.json();
       return userInfo;
     }
 
-    // This function gets the shield from shields.io and returns it
-    async function makeShield(t, p) {
-      let logoColor;
+    // get the shield from shields.io and returns it
+    async function makeShield() {
+      const { t, p } = await getUserInfo();
+      // a lookup table for the presence colors
+      const presenceColors = {
+        online: "3ba55d",
+        idle: "faa81a",
+        "do not disturb": "ed4245",
+        offline: "555",
+      };
 
-      if (req.query.logoColor) {
-        if (req.query.logoColor === "presence") {
-          logoColor = await getPresenceColor();
-        } else {
-          logoColor = req.query.logoColor;
-        }
+      // a lookup table for the themes
+      const themes = {
+        default: ["555", "5865F2"],
+        "default-inverted": ["5865F2", "555"],
+        clean: ["555", presenceColors[p]],
+        "clean-inverted": [`${presenceColors[p]}`, "555"],
+        discord: ["5865F2", presenceColors[p]],
+        "discord-inverted": [presenceColors[p], "5865F2"],
+        "full-presence": [presenceColors[p], presenceColors[p]],
+        gray: ["555", "555"],
+        grey: ["555", "555"],
+        blurple: ["5865F2", "5865F2"],
+      };
+
+      
+
+      if (logoColor) {
+        logoColor = logoColor === "presence" ? presenceColors[p] : logoColor;
       } else {
-        if (req.query.style === "social") {
-          logoColor = "#5865F2";
-        } else {
-          logoColor = "white";
-        }
+        logoColor = style === "social" ? "#5865F2" : "white";
       }
 
-      // This function replaces the selected color with one depending on the presence information
-      async function getPresenceColor() {
-        switch (p) {
-          case "online": {
-            return "37b05d";
-          }
-          case "idle": {
-            return "faa81a";
-          }
-          case "do not disturb": {
-            return "ed4245";
-          }
-          default: {
-            return "555";
-          }
-        }
-      }
+      const colors = themes[theme] || themes["default"];
+      console.log(colors)
 
-      // presenceTheme is present for legacy purposes, theme should be used instead
-      let theme = req.query.presenceTheme || req.query.theme;
-
-      let rightColor, leftColor;
-
-      // This is where the whole theming thing takes place!
-      switch (theme) {
-        default:
-          {
-            rightColor = "555";
-            leftColor = "7289da";
-          }
-          break;
-        case "default-inverted":
-          {
-            leftColor = "555";
-            rightColor = "7289da";
-          }
-          break;
-        case "clean":
-          {
-            leftColor = "555";
-            rightColor = await getPresenceColor();
-          }
-          break;
-        case "clean-inverted":
-          {
-            leftColor = await getPresenceColor();
-            rightColor = "555";
-          }
-          break;
-
-        case "dc":
-        case "discord":
-          {
-            leftColor = "7289da";
-            rightColor = await getPresenceColor();
-          }
-          break;
-        case "dc-inverted":
-        case "discord-inverted":
-          {
-            leftColor = await getPresenceColor();
-            rightColor = "7289da";
-          }
-          break;
-        case "full":
-        case "full-presence":
-          {
-            leftColor = await getPresenceColor();
-            rightColor = await getPresenceColor();
-          }
-          break;
-        case "gray":
-        case "grey":
-          {
-            leftColor = "555";
-            rightColor = "555";
-          }
-          break;
-        case "blurple":
-          {
-            leftColor = "7289da";
-            rightColor = "7289da";
-          }
-          break;
-      }
-
-      let shieldURL;
-
-      if (req.query.compact === "true") {
-        shieldURL = `https://img.shields.io/static/v1?label=&message=${encodeURIComponent(
-          t
-        )}&style=${
-          req.query.style || "for-the-badge"
-        }&color=${rightColor}&labelColor=${leftColor}&logo=discord&logoColor=${logoColor}`;
+      let text;
+      // if compact is true, we set the text accordingly
+      if (compact) {
+        text = {
+          label: "",
+          message: encodeURIComponent(t),
+        };
       } else {
-        shieldURL = `https://img.shields.io/static/v1?label=${encodeURIComponent(
-          t
-        )}&message=${encodeURIComponent(p)}&style=${
-          req.query.style || "for-the-badge"
-        }&color=${rightColor}&labelColor=${leftColor}&logo=discord&logoColor=${logoColor}`;
+        text = {
+          label: encodeURIComponent(t),
+          message: encodeURIComponent(p),
+        };
       }
 
+      const shieldURL = `https://img.shields.io/static/v1?label=${text.label}&message=${text.message}&style=${style}&color=${colors[0]}&labelColor=${colors[1]}&logo=discord&logoColor=${logoColor}`;
       const rawShield = await fetch(shieldURL);
-
       const svgShield = await rawShield.text();
 
-      /* The for-the-badge style makes the username uppercase, and we don't want that since Discord usernames
-       * are case sensitive; here we replace it with the correctly capitalized text */
-      const usernameRegExp = new RegExp(`${t.toUpperCase()}`, "g");
-      let svgShieldFix = svgShield.replace(usernameRegExp, t);
+      // fix the username and invite url being capitalized
+      let svgShieldFix = svgShield.replaceAll(t.toUpperCase(), t);
+      svgShieldFix = svgShieldFix.replaceAll(
+        "HTTPS://DISCORD.GG/ZKSPFFWQDG",
+        "https://discord.gg/zkspfFwqDg"
+      );
 
-      /* Here we fix the link to the discord server being bolded.
-      This fix is jank, but so is the rest of the codebase honestly. Might rewrite from scratch soon. */
-      const inviteRegExp = new RegExp("HTTPS://DISCORD.GG/ZKSPFFWQDG", "g");
-      svgShieldFix = svgShieldFix.replace(inviteRegExp, "https://discord.gg/zkspfFwqDg");
-
-      // Here we make the username bold
-      const boldRegEx = new RegExp(`fill="#fff">${t}</text>`, "g");
-      svgShieldFix = svgShieldFix.replace(
-        boldRegEx,
+      // make the username bold
+      svgShieldFix = svgShieldFix.replaceAll(
+        `fill="#fff">${t}</text>`,
         `fill="#fff" font-weight="bold">${t}</text>`
       );
 
@@ -156,11 +98,9 @@ export default async function handler(req, res) {
       res.status(200).send(svgShieldFix);
     }
 
-    // We call all the functions and finally resolve the promise we created at the beginning
-    getUserInfo(req.query.user).then((userInfo) => {
-      makeShield(userInfo.t, userInfo.p).then(() => {
-        resolve;
-      });
+    // call the function and resolve the promise
+    makeShield().then(() => {
+      resolve;
     });
   });
 }
