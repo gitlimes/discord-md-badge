9ea62bb635c7b9c1f2a0508e0d044e1d68038485
Ash Monty <git@ashmonty.com>
2022-06-16 12:01:04 +0200

    clean up bot code

--- bot/index.js
+++ bot/index.js
@@ -7,11 +7,12 @@ const port = 3581;
 
 client.login(process.env.DC_TOKEN);
 
-app.get("/md-shield/json/", (req, res) => {
+function getUserInfo(req, res, isBot) {
   const userID = req.query.u;
 
-  // This makes it easier to deploy this without needing to edit the code manually
-  const guildID = process.env.GUILD_ID || "852978546187698206";
+  // If the user is a bot, we check the bot server, else the regular server. The GUILD_ID env var overrides this
+  const guildID =
+    process.env.GUILD_ID || isBot ? "867345122941337610" : "852978546187698206";
 
   const guild = client.guilds.cache.get(guildID);
 
@@ -21,7 +22,7 @@ app.get("/md-shield/json/", (req, res) => {
     let presence = user.presence.status;
     const tag = user.tag;
 
-    // If the presence is dnd we rename it to Do Not Disturb
+    // If the presence is dnd we rename it to do not disturb
     if (presence === "dnd") {
       presence = "do not disturb";
     }
@@ -29,40 +30,19 @@ app.get("/md-shield/json/", (req, res) => {
     res.status(200).json({ t: tag, p: presence });
   } else {
     // If the user isn't in the guild, we tell them to join, since we can't get the info otherwise
-    res.status(418).json({
+    res.status(200).json({
       t: "Error",
       p: "You have to join the Discord server (https://discord.gg/zkspfFwqDg) for this to work.",
     });
   }
+}
+
+app.get("/md-shield/json/", (req, res) => {
+  getUserInfo(req, res);
 });
 
 app.get("/md-shield/bot/json/", (req, res) => {
-  const userID = req.query.u;
-
-  // This makes it easier to deploy this without needing to edit the code manually
-  const guildID = process.env.GUILD_ID || "867345122941337610";
-
-  const guild = client.guilds.cache.get(guildID);
-
-  // We check if the bot is in the bot server (private)
-  if (guild.member(userID)) {
-    const user = client.users.cache.get(userID);
-    let presence = user.presence.status;
-    const tag = user.tag;
-
-    // If the presence is dnd we rename it to Do Not Disturb
-    if (presence === "dnd") {
-      presence = "do not disturb";
-    }
-
-    res.status(200).json({ t: tag, p: presence });
-  } else {
-    // If the bot isn't in the guild, we tell the dev to send me the invite, since we can't get the info otherwise
-    res.status(418).json({
-      t: "Error",
-      p: "For this to work, you need to DM me (monty#9398) the invite for your bot.",
-    });
-  }
+  getUserInfo(req, res, true);
 });
 
 client.on("ready", () => {
@@ -70,3 +50,4 @@ client.on("ready", () => {
     console.log(`Listening on http://localhost:${port}`);
   });
 });
+ì
